f26e956c5322429245ba368d6619e4a0315a1b2e
yusufriswan <119690890+yusufriswan@users.noreply.github.com>
2026-01-04 20:23:35 +0530

    Update allowedOrigins.js

--- backend/src/config/allowedOrigins.js
+++ backend/src/config/allowedOrigins.js
@@ -1,7 +1,5 @@
 const allowedOrigins = [
-    'http://localhost:3000',
-    'https://www.dandrepairshop.com',
-    'https://dandrepairshop.com'
+    'https://front-log-lake.vercel.app'
 ]
 
 module.exports = allowedOrigins
